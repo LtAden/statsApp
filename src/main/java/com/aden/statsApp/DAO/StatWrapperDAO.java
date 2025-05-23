b2acb0993e7fca0c82111e3bc1f550f6d5b67f97
LtAden <DarthTradeous1@wp.pl>
2025-05-23 19:24:32 +0200

    Added support for custom stat file location settable via application.properties

--- src/main/java/com/aden/statsApp/DAO/StatWrapperDAO.java
+++ src/main/java/com/aden/statsApp/DAO/StatWrapperDAO.java
@@ -2,6 +2,7 @@ package com.aden.statsApp.DAO;
 
 import com.aden.statsApp.model.StatWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -9,14 +10,17 @@ import java.io.IOException;
 
 @Repository
 public class StatWrapperDAO {
+
+    @Value("${stat.file.path:stats.json}")
+    private String statFileLocation;
     ObjectMapper objectMapper = new ObjectMapper();
     public void saveStatWrapperToFile(StatWrapper statWrapper) throws IOException {
-        objectMapper.writeValue(new File("stats.json"), statWrapper);
+        objectMapper.writeValue(new File(statFileLocation), statWrapper);
     }
 
     public StatWrapper readWrapperFromFile() {
         try {
-            return objectMapper.readValue(new File("stats.json"), StatWrapper.class);
+            return objectMapper.readValue(new File(statFileLocation), StatWrapper.class);
         } catch (IOException e) {
             System.out.println("Failed to read from file, file may be missing or damaged? Creating new StatWrapper");
             return new StatWrapper();
